8f52466e1e4139b9b75461f0350e9292f6735b5d
23Adarsh <107578738+23Adarsh@users.noreply.github.com>
2022-07-22 22:10:58 +0530

    Add files via upload

---
+++ DOB.java
@@ -0,0 +1,9 @@
+public class DOB {
+    public static void main(String[] args)
+    {
+        System.out.println("Name : Adarsh Kumar Singh");
+        System.out.println("DOB : 06-07-1999");
+        System.out.println("MOB : 63388585861");
+    }
+    
+}
