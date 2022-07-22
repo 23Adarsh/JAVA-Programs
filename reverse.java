85c3d8e0ae9650dd11e543f8555be49bba29f0f2
23Adarsh <107578738+23Adarsh@users.noreply.github.com>
2022-07-22 23:21:25 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,17 @@
+public class reverse {
+    public static void main(String[] args)
+    {
+        for(int i =10;i>0;i--)
+        {
+            if(i%2==0)
+            {
+            System.out.print(i+" Even");}
+            else{
+                System.out.print(i+" Odd");
+            }System.out.println();
+        }
+    
+    }
+
+    
+}
