9d2097e4c4e39aa8a907c951f1da4c66fedd712c
23Adarsh <107578738+23Adarsh@users.noreply.github.com>
2022-07-22 22:13:08 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,19 @@
+import javax.lang.model.util.ElementScanner14;
+
+public class even {
+    public static void main(String[] args)
+    {
+        for(int i =1;i<=10;i++)
+        
+        
+        if(i%2==0)
+        {
+            System.out.println(""+i+" Even");
+        }
+        else 
+        {
+            System.out.println(""+i+" odd");
+        }
+         
+    }
+}
