fbb7f0565f4f07f1b68c4cbbf4e920530c1ea466
Adarsh Kumar Singh <107578738+23Adarsh@users.noreply.github.com>
2022-08-25 16:10:54 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,13 @@
+public class Swap {
+    public static void main(String[] args)
+    {
+        int a=10,b=0;
+  
+        System.out.println("Two Number Swapping W/o Third Varriable ");
+        a=a+b;
+        b=a-b;
+        a= a-b;
+
+        System.out.print(" Two Number Swapped Value -: "+a+" "+b);
+    }
+}
